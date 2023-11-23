8ed374850ca993e1484b836ce9245e0421c2b17a
Felisberto3 <felisbertoalberto183@gmail.com>
2023-11-23 05:10:43 +0100

    file js

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 const touch = document.querySelector('.sidebar .header .barIcon')
 
 touch.onclick = () => {
-    document.querySelector('.sidebar').classList.toggle('sidebarClosed')
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.classList.toggle('sidebarClosed')
+
+    const spans = document.querySelectorAll('.sidebar span')
+    const isSideBardCloset = sidebar.classList.contains('sidebarClosed')
+
+    if (isSideBardCloset) {
+        spans.forEach(span=> span.style.display="none") 
+    }
+    if (!isSideBardCloset) {
+        spans.forEach(span=> span.style.display="block") 
+    }
 }
+const touchIcon = document.querySelectorAll('.sidebar li i')
+
+touchIcon.forEach(icon=>{
+    // console.log(icon.);
+})
