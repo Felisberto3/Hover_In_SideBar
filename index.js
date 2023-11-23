1284a4d9d20980e54f724b62eb5d7d2d68edd149
Felisberto3 <felisbertoalberto183@gmail.com>
2023-11-23 04:53:59 +0100

    file js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const touch = document.querySelector('.sidebar .header .barIcon')
 
 touch.onclick = () => {
-    document.querySelectorAll('.sidebar li span').style.display="none"
+    document.querySelector('.sidebar').classList.toggle('sidebarClosed')
 }
